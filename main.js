0d0a124581e4dc63938c7ff0784f53c27a81ad87
LanchenbiKonthoujam <97613330+LanchenbiKonthoujam@users.noreply.github.com>
2022-05-06 19:54:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function modelDone(){
+    console.log("PoseNet Is Initialized And Loaded");
+}
+
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
+
+        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
+        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+    }
+}
